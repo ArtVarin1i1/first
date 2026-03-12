1a52ee23375395bf7f7468b14d6f18e3250ce0cf
artvarin11 <artvarin11@gmail.com>
2026-03-12 21:17:21 +0300

    asddasd

--- sdad.py
+++ sdad.py
@@ -5,3 +5,4 @@ url='https://www.cbr-xml-daily.ru/daily_json.js'
 response=requests.request('GET',url)
 data=json.loads(response.text)
 print(str(data['Valute']['USD']['Name'])+str(':'),str(round(data['Valute']['USD']['Value'],2)))
+print()
